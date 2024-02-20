c7bbee48d69cb6851c07e8a0200d1c4147f11c8f
Sanjay S <125551135+SanjaySk7@users.noreply.github.com>
2024-02-21 00:36:25 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This is a basic social media app for post update only in text.</p>
+        </main>
+    )
+}
+
+export default About
---
+++ src/App.js
@@ -0,0 +1,97 @@
+import Layout from './Layout';
+import Home from './Home';
+import NewPost from './NewPost';
+import PostPage from './PostPage';
+import About from './About';
+import Missing from './Missing';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+
+function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "January 12, 2024 11:17:36 AM",
+      body: "Hello guys, I'm Sanjay S. This is my 1st post"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "January 13, 2024 10:45:14 AM",
+      body: "This is my second post.I had attended one interview on Virtusa"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "January 15, 2024 12:00:00 AM",
+      body: "Happy Pongal to all guys!!!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "January 19, 2024 09:10:46 AM",
+      body: "Feeling sad. Holiday over back to college!!"
+    }
+  ])
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [postTitle, setPostTitle] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const filteredResults = posts.filter((post) =>
+      ((post.body).toLowerCase()).includes(search.toLowerCase())
+      || ((post.title).toLowerCase()).includes(search.toLowerCase()));
+
+    setSearchResults(filteredResults.reverse());
+  }, [posts, search])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, datetime, body: postBody };
+    const allPosts = [...posts, newPost];
+    setPosts(allPosts);
+    setPostTitle('');
+    setPostBody('');
+    navigate('/');
+  }
+
+  const handleDelete = (id) => {
+    const postsList = posts.filter(post => post.id !== id);
+    setPosts(postsList);
+    navigate('/');
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout
+        search={search}
+        setSearch={setSearch}
+      />}>
+        <Route index element={<Home posts={searchResults} />} />
+        <Route path="post">
+          <Route index element={<NewPost
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
+          />} />
+          <Route path=":id" element={<PostPage
+            posts={posts}
+            handleDelete={handleDelete}
+          />} />
+        </Route>
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
